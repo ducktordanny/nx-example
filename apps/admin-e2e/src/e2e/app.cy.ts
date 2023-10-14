5edf7c5d9239dbeffe606fcfe2b12a51338b24b6
Daniel Lazar <lazardani0112@gmail.com>
2023-10-14 22:53:53 +0200

    chore(apps): rename frontend apps

--- apps/admin-e2e/src/e2e/app.cy.ts
+++ apps/admin-e2e/src/e2e/app.cy.ts
@@ -1,4 +1,4 @@
-import { getGreeting } from '../support/app.po';
+import {getGreeting} from '../support/app.po';
 
 describe('manage', () => {
   beforeEach(() => cy.visit('/'));
--- apps/posts/jest.config.ts
+++ apps/posts/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  displayName: 'manage',
+  displayName: 'posts',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/manage',
+  coverageDirectory: '../../coverage/apps/posts',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
