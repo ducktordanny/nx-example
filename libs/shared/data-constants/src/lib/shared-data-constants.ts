9f35c9517ad188bc25648a2f8b496506f2283b35
Daniel Lazar <lazardani0112@gmail.com>
2023-10-17 22:47:21 +0200

    refact(shared-data-constants): separate version into accessible const

--- libs/shared/data-constants/src/lib/shared-data-constants.ts
+++ libs/shared/data-constants/src/lib/shared-data-constants.ts
@@ -1,7 +1,9 @@
 import {IWelcome} from '@nx-example/shared/data-api-interfaces';
 
+export const version = '0.0.0';
+
 export const welcomeResponse: IWelcome = {
   message: 'Hello API',
   description: 'This is a small example NestJS API for representing Nx features.',
-  version: '0.0.0',
+  version,
 };
